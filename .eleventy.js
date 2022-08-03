d7801e9dfd76279669bcb375ab2fc6eaed7786ff
Nolan <nolandonahue0@gmail.com>
2022-08-03 08:47:16 -0400

    asdf

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 const { DateTime } = require("luxon");
 
-// Tell eleventy where the front facing files are going to be
 module.exports = function (eleventyConfig) {
-  //Grab the files for eleventy to take and pass through to the public folder
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
@@ -10,12 +8,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
-      input: "src", //Change this for where the input files are
-      output: "public", //This can also be changed
+      input: "src",
+      output: "public",
     },
   };
 };
